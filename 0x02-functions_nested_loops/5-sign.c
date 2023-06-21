c0faade876796347da21d685086386625563a187
mcdua <caicastro.gh@gmail.com>
2023-06-20 19:49:46 -0700

    Sign updated

--- 0x02-functions_nested_loops/5-sign.c
+++ 0x02-functions_nested_loops/5-sign.c
@@ -2,27 +2,27 @@
 
 /**
  * print_sign - Determines if the input number
- * greater, iqual or less than zero.
- * @n: the input number as an integer
- * Return: 1 is greater than zero. 0 is zero
- * -1 is less than zero.
+ * is greater, equal to, or less than zero.
+ * @n: The input number as an integer.
+ * Return: 1 if greater than zero, 0 if zero,
+ *         -1 if less than zero.
  */
 int print_sign(int n)
 {
-	if (n > 0)
-	{
-		_putchar(43);
-		return (1);
-	}
-	else if (n > 0)
-	{
-		_putchar(45);
-		return (-1);
-	}
-	else
-	{
-		_putchar(48);
-		return (0);
-	}
-	_putchar('\n');
+    if (n > 0)
+    {
+        _putchar(43);  /* '+' character */
+        return (1);
+    }
+    else if (n < 0)
+    {
+        _putchar(45);  /* '-' character */
+        return (-1);
+    }
+    else
+    {
+        _putchar(48);  /* '0' character */
+        return (0);
+    }
+    _putchar('\n');
 }
